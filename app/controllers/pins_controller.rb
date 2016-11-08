bbb7ac0552d98289cccee884237e7e0dc776c202
gshekar5017 <shekargudepu@gmail.com>
2016-11-08 06:56:51 +0000

    Basic Styling

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :find_pin, only: [:show,:edit,:update,:destroy]
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   def index
     @pins= Pin.all.order("created_at DESC")
   end
